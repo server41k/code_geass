d3c5189a8bcda71fcb1625fc8ae8f5d3c0f39e21
server41k <chaush.s.i20@gmail.com>
2022-04-02 16:22:28 +0300

    4.2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,16 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
-
+/* Страница lelouch */
+router.get('/lelouch', function (req, res, next) {
+    res.send("<h1>Страница lelouch</h1>")
+});
+/* Страница  cc */
+router.get('/cc', function (req, res, next) {
+    res.send("<h1>Страница C.C.</h1>")
+});
+/* Страница zero */
+router.get('/zero', function (req, res, next) {
+    res.send("<h1>Страница zero</h1>")
+});
 module.exports = router;
