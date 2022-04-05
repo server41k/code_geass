fb659f9949b480ca6405cb0efa49769134744b82
server41k <chaush.s.i20@gmail.com>
2022-04-05 13:37:42 +0300

    6.2

--- createDB.js
+++ createDB.js
@@ -2,7 +2,7 @@ const MongoClient = require("mongodb").MongoClient;
 
 const url = "mongodb://localhost:27017/";
 const mongoClient = new MongoClient(url);
-
+var data = require("./data.js").data
 // Подключаемся к серверу
 mongoClient.connect(function (err, client) {
 
