da83924e346f74a350a40349c598ec67c481c308
server41k <chaush.s.i20@gmail.com>
2022-04-05 13:42:33 +0300

    6.3

--- createDB.js
+++ createDB.js
@@ -5,11 +5,11 @@ const mongoClient = new MongoClient(url);
 var data = require("./data.js").data
 // Подключаемся к серверу
 mongoClient.connect(function (err, client) {
-
     // обращаемся к базе данных admin
     const db = client.db("code_geass");
+    db.dropDatabase();
     var collection = db.collection("heroes");
-    collection.insertOne({ name: "Lelouch" }, function (err, result) {
+    collection.insertMany(data, function (err, result) {
         client.close();
     })
 })
