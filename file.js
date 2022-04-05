fd6e9988c1b62365283b231e97ed586b2cd9576c
server41k <chaush.s.i20@gmail.com>
2022-04-05 14:15:50 +0300

    7.2

--- file.js
+++ file.js
@@ -1,19 +1,19 @@
 var async = require("async")
-var arr = [1, 2, 3, 4, 5]
-async.each(
-    arr,
-    function (item, callback) {
-        if (typeof item === "number") {
-            callback()
-        } else {
-            callback("Нашли не число")
-        }
-    },
-    function (err) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log("Проверка прошла успешно. Все элементы массива являются числами")
-        }
-    }
-)
+async.series([
+    firstWord,
+    secondWord,
+    thirdWord
+],
+    function (err, result) {
+        if (err) throw err;
+        console.log(result.join(' '))
+    });
+function firstWord(callback) {
+    callback(null, 'МАМА')
+}
+function secondWord(callback) {
+    callback(null, 'МЫЛА')
+}
+function thirdWord(callback) {
+    callback(null, 'РАМУ')
+}
